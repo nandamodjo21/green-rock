eec35c442c44f588b9375b8af916b8bdce1961fd
nandamodjo21 <jefrandamodjo.mhs20@ti.poligon.ac.id>
2023-09-06 00:22:37 +0800

    fix: update view barang

--- application/views/admin/barang.php
+++ application/views/admin/barang.php
@@ -9,7 +9,8 @@
             <div class="card shadow mb-4">
                 <div class="card-header py-3">
 
-                    <a href="" class="btn btn-primary mb-3" data-toggle="modal" data-target="#tambahModal">Tambah Data</a>
+                    <a href="" class="btn btn-primary mb-3" data-toggle="modal" data-target="#tambahModal">Tambah
+                        Data</a>
                 </div>
                 <div class=" card-body">
 
@@ -28,20 +29,22 @@
                             <tbody>
                                 <?php $no = 1;
                                 foreach ($barang as $br) : ?>
-                                    <tr>
-                                        <td scope="row"><?= $no++; ?></td>
-                                        <td><?= $br->nama_barang ?></td>
-                                        <td><?= $br->stok_barang ?></td>
-                                        <td><?= $br->harga_barang ?></td>
-                                        <td>
+                                <tr>
+                                    <td scope="row"><?= $no++; ?></td>
+                                    <td><?= $br->nama_barang ?></td>
+                                    <td><?= $br->stok_barang ?></td>
+                                    <td><?= $br->harga_barang ?></td>
+                                    <td>
 
-                                            <a href="" class="badge badge-success" data-toggle="modal" data-target="#editbarang<?= $br->id ?>">edit</a>
-                                            <a href="<?php echo base_url('Barang/delete/' . $br->id) ?>" class="badge badge-danger">delete</a>
-                                        </td>
+                                        <a href="" class="badge badge-success" data-toggle="modal"
+                                            data-target="#editbarang<?= $br->id ?>">edit</a>
+                                        <a href="<?php echo base_url('Barang/delete/' . $br->id) ?>"
+                                            class="badge badge-danger">delete</a>
+                                    </td>
 
-                                    </tr>
+                                </tr>
                             </tbody>
-                        <?php endforeach; ?>
+                            <?php endforeach; ?>
                         </table>
 
                     </div>
@@ -54,7 +57,8 @@
 
 
 <!-- Modal -->
-<div class="modal fade" id="tambahModal" tabindex="-1" role="dialog" aria-labelledby="tambahModalLabel" aria-hidden="true">
+<div class="modal fade" id="tambahModal" tabindex="-1" role="dialog" aria-labelledby="tambahModalLabel"
+    aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
@@ -89,41 +93,42 @@
 </div>
 <?php $no = 1;
 foreach ($barang as $br) : ?>
-    <!-- Modal edit-->
-    <div class="modal fade" id="editbarang<?= $br->id ?>" tabindex="-1" role="dialog" aria-labelledby="editbarangLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="editbarangLabel">Form Edit</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <form action="<?= base_url('Barang/edit_barang'); ?>" method="post">
-                    <div class="modal-body">
-                        <div class="form-group">
-                            <input type="hidden" value="<?= $br->id ?>" name="id" class="form-control">
-                        </div>
-                        <div class="form-group">
-                            <label>Nama Barang</label>
-                            <input type="text" value="<?= $br->nama_barang ?>" name="nama" class="form-control">
-                        </div>
-                        <div class="form-group">
-                            <label>Stok</label>
-                            <input type="text" value="<?= $br->stok_barang ?>" name="stok" class="form-control">
-                        </div>
-                        <div class="form-group">
-                            <label>Harga</label>
-                            <input type="text" value="<?= $br->harga_barang ?>" name="harga" class="form-control">
-                        </div>
-
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
-                        <button type="submit" class="btn btn-primary">Simpan</button>
-                    </div>
-                </form>
+<!-- Modal edit-->
+<div class="modal fade" id="editbarang<?= $br->id ?>" tabindex="-1" role="dialog" aria-labelledby="editbarangLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="editbarangLabel">Form Edit</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
             </div>
+            <form action="<?= base_url('Barang/edit_barang'); ?>" method="post">
+                <div class="modal-body">
+                    <div class="form-group">
+                        <input type="hidden" value="<?= $br->id ?>" name="id" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label>Nama Barang</label>
+                        <input type="text" value="<?= $br->nama_barang ?>" name="nama" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label>Stok</label>
+                        <input type="text" value="<?= $br->stok_barang ?>" name="stok" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label>Harga</label>
+                        <input type="text" value="<?= $br->harga_barang ?>" name="harga" class="form-control">
+                    </div>
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+                    <button type="submit" class="btn btn-primary">Simpan</button>
+                </div>
+            </form>
         </div>
     </div>
+</div>
 <?php endforeach; ?>
