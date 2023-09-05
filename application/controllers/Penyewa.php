8d1d3acf55a4bc00cd6c0f5f96d12e7936fb0a2c
nandamodjo21 <jefrandamodjo.mhs20@ti.poligon.ac.id>
2023-09-06 00:21:51 +0800

    fix: update penyewaan controller

--- application/controllers/Penyewa.php
+++ application/controllers/Penyewa.php
@@ -13,6 +13,22 @@ class Penyewa extends CI_Controller
         $data['title'] = 'Penyewa';
         $data['user'] = $this->db->get_where('t_user', ['email' => $this->session->userdata('email')])->row_array();
 
+        // $data['penyewa'] = $this->M_penyewa->get_data();
+
+        // $dataFromApi = 
+        // $apiUrl = 'http://d417-182-1-137-147.ngrok-free.app/api/'; // Ganti dengan URL sesuai dengan REST API Anda
+    
+        // // Buat request cURL
+        // $curl = curl_init($apiUrl);
+        
+        // // Konfigurasi request cURL
+        // curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        
+        // // Eksekusi request
+        // $data['api'] = curl_exec($curl);
+        
+        // // Tutup koneksi cURL
+        // curl_close($curl);
         $data['penyewa'] = $this->M_penyewa->get_data();
         $this->load->view('templates/header', $data);
         $this->load->view('templates/sidebar', $data);
@@ -20,4 +36,15 @@ class Penyewa extends CI_Controller
         $this->load->view('admin/penyewa', $data);
         $this->load->view('templates/footer');
     }
+    private function getDataFromApi()
+    {
+        // Endpoint URL dari REST API
+       
+    
+        return $response;
+        // Proses respons dari REST API
+       
+    }
+    
+    
 }
